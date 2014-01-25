4dd04f62215deec6399e0461a062db97205b0b0b
Zach Lubinski <zachl@shaw.ca>
2014-01-25 15:02:06 -0700

    - New naming convention: push/pull mechanic is now called tractor beam for sake of sanity. - Unbound the jump button from tractor beam. Tractor beam is now controlled by left click and right click. - Split tractor beam behavior from movement script into it's own script. - Fixed tractor beam so it now affects all objects it intersects. - Added "stasis effect" to tractor beam. How the tractor beam worked before was it added force each time it hit an object, causing objects to accelerate very quickly. This also meant if an object was already moving very quickly the stasis beam wouldn't affect it much. The stasis effect resets the objects movement at each hit, so if an object is already moving the beam will now "grab" it and completely nullify the previous movement. This also reduces the acceleration on repeated beam hits, giving the beam a more controlled feeling. - Added power and stasisEffect parameters to tractor beam, if stasis is turned off, the power will have a much stronger effect.

---
+++ Assets/TractorBeam.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TractorBeam : MonoBehaviour {
+	public int power;
+	public bool stasisEffect;
+	void FixedUpdate () {
+		if (Input.GetButton("Fire2") || Input.GetButton("Fire1")) {
+			Vector3 clickLoc = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			Vector2 myLoc = new Vector2(transform.position.x,transform.position.y);
+			//raycast needs a vector for the direction from the player.
+			Vector2 target_direction = new Vector2(clickLoc.x - myLoc.x,clickLoc.y - myLoc.y); 
+			//print ("target_direction " + target_direction);
+			//print ("myLoc " + myLoc);
+			if( Input.GetButton("Fire2"))
+				Debug.DrawRay(myLoc,target_direction, Color.blue,1);
+			if(Input.GetButton ("Fire1"))
+				Debug.DrawRay(myLoc,target_direction, Color.green,1);
+			//send a raycast and return all intersections. magn. gives distance to cast e.g. distance clicked from player
+			RaycastHit2D[] all_hit = Physics2D.RaycastAll(myLoc,target_direction,target_direction.magnitude);
+			foreach(RaycastHit2D hit in all_hit){
+				if(hit.collider != null){
+					//print (hit.collider.gameObject.name);
+					if (hit.collider.gameObject.name != "Prototypi" && hit.collider.rigidbody2D != null){
+						int dir = 1;
+						if (Input.GetButton("Fire1")) dir = -1;
+						if(stasisEffect) hit.collider.rigidbody2D.velocity = Vector2.zero; //reset velocity for stasis effect
+						hit.collider.rigidbody2D.AddForce( (myLoc - target_direction) * dir * power);
+					}
+				}
+			}
+		}
+	}
+}

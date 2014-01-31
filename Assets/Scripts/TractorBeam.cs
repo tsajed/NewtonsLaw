4b5f809aa170221939ff255b116f574af45c38c9
Solanar <and_555@hotmail.com>
2014-01-30 20:29:25 -0700

    the pull towards the player seems to be stronger than the push

--- Assets/Scripts/TractorBeam.cs
+++ Assets/Scripts/TractorBeam.cs
@@ -1,11 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class TractorBeam : MonoBehaviour {
-	public int power;
+public class TractorBeam : MonoBehaviour 
+{
+	public float power;
 	public bool stasisEffect;
-	void FixedUpdate () {
-		if (Input.GetButton("Fire2") || Input.GetButton("Fire1")) {
+
+	void FixedUpdate () 
+	{
+		if (Input.GetButton("Fire2") || Input.GetButton("Fire1")) 
+		{
 			Vector3 clickLoc = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 			Vector2 myLoc = new Vector2(transform.position.x,transform.position.y);
 			//raycast needs a vector for the direction from the player.
@@ -18,13 +22,20 @@ public class TractorBeam : MonoBehaviour {
 				Debug.DrawRay(myLoc,target_direction, Color.green,1);
 			//send a raycast and return all intersections. magn. gives distance to cast e.g. distance clicked from player
 			RaycastHit2D[] all_hit = Physics2D.RaycastAll(myLoc,target_direction,target_direction.magnitude);
-			foreach(RaycastHit2D hit in all_hit){
-				if(hit.collider != null){
+			foreach(RaycastHit2D hit in all_hit)
+			{
+				if(hit.collider != null)
+				{
 					//print (hit.collider.gameObject.name);
-					if (hit.collider.gameObject.name != "Prototypi" && hit.collider.rigidbody2D != null){
-						int dir = 1;
-						if (Input.GetButton("Fire1")) dir = -1;
-						if(stasisEffect) hit.collider.rigidbody2D.velocity = Vector2.zero; //reset velocity for stasis effect
+					if (hit.collider.gameObject.name != "Prototypi" && hit.collider.rigidbody2D != null
+						&& !hit.collider.gameObject.name.Contains("Projectile"))
+					{
+						float dir = 1 * 0.5f; // .5 because the grab seems to be op
+						if (Input.GetButton ("Fire1"))
+							dir = -1;
+						Debug.Log ("p : " + power);
+						if (stasisEffect)
+							hit.collider.rigidbody2D.velocity = Vector2.zero; //reset velocity for stasis effect
 						hit.collider.rigidbody2D.AddForce( (myLoc - target_direction) * dir * power);
 					}
 				}

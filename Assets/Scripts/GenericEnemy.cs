ad3b376d6c1986c81e1ee7b8fb5c12cd94c02603
Solanar <and_555@hotmail.com>
2014-01-30 20:53:40 -0700

    c# > java

--- Assets/Scripts/GenericEnemy.cs
+++ Assets/Scripts/GenericEnemy.cs
@@ -1,16 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GenericEnemy {
-
+public class GenericEnemy 
+{
 	public GameObject self;
 
-	private int health = 100;
-	private float movementSpeed = 2.0f;
-	private float damage = 2.0f;
+	public int health { get; set; }
+	public float movementSpeed { get; set; }
+	public float damage { get; set; }
+	public float maxSpeed = 5f;				// The fastest the player can travel in the x axis.
 
-
-	public GenericEnemy(GameObject self, int health, float speed, float damage) {
+	public GenericEnemy(GameObject self, int health, float speed, float damage) 
+	{
 		this.self = self;
 		this.health = health;
 		this.movementSpeed = speed;
@@ -18,42 +19,15 @@ public class GenericEnemy {
 	}
 
 	// Flips the Sprite so it looks in the opposite direction
-	public void Flip() {
+	public void Flip()
+	{
 		// Multiply the x component of localScale by -1.
 		Vector3 enemyScale = self.transform.localScale;
 		enemyScale.x *= -1;
 		self.transform.localScale = enemyScale;
 	}
 
-	public float getSpeed() {
-		return movementSpeed;
-	}
+	public void increaseHealth(int amount) { health += amount; }
 
-	public float getDamage() {
-		return damage;
-	}
-
-	public int getHealth() {
-		return health;
-	}
-
-	public void increaseHealth(int amount) {
-		health += amount;
-	}
-
-	public void decreaseHealth(int amount) {
-		health -= amount;
-	}
-
-	public void setDamage(float newDamage) {
-		damage = newDamage;
-	}
-
-	public void setHealth(int newHealth) {
-		health = newHealth;
-	}
-
-	public void setSpeed(float newSpeed) {
-		movementSpeed = newSpeed;
-	}
+	public void decreaseHealth(int amount) { health -= amount; }
 }

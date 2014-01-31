f80401377819bf5cfe0bcdf2c986801dad1a7e89
Solanar <and_555@hotmail.com>
2014-01-30 20:53:17 -0700

    formatting

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,21 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Movement : MonoBehaviour {
-
+public class Movement : MonoBehaviour 
+{
 	public float moveForce = 365f;			// Amount of force added to move the player left and right.
 	public float maxSpeed = 5f;				// The fastest the player can travel in the x axis.
+
 	// Use this for initialization
-	void Start () {
-	
-	}
+	void Start () {}
 	
 	// Update is called once per frame
-	void FixedUpdate () {
+	void FixedUpdate () 
+	{
 		// Cache the horizontal input.
 		float h = Input.GetAxis("Horizontal");
 		float v = Input.GetAxis("Vertical");
-
+		
 		// If the player is changing direction (h has a different sign to velocity.x) or hasn't reached maxSpeed yet...
 		if(h * rigidbody2D.velocity.x < maxSpeed)
 			// ... add a force to the player.
@@ -32,7 +32,5 @@ public class Movement : MonoBehaviour {
 
 		if(Mathf.Abs(rigidbody2D.velocity.y) > maxSpeed)
 			rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, Mathf.Sign(rigidbody2D.velocity.y) * maxSpeed);
-
-
 	}
 }

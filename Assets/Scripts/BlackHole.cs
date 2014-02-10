055e35933f4df617797abe2da4269d5f532edbb7
Eric Lam <elam3@ualberta.ca>
2014-02-10 12:09:43 -0700

    Started Black Holes' implementation. Created a prefab and edited animations related to the black hole.

---
+++ Assets/Scripts/BlackHole.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlackHole : MonoBehaviour {
+
+	float force = 25.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 center = renderer.bounds.center;
+		Collider2D[] colliders = Physics2D.OverlapCircleAll(renderer.bounds.center, renderer.bounds.extents.x);
+		Debug.Log(renderer.bounds.center);
+		Debug.Log(renderer.bounds.extents.x);
+		foreach(Collider2D hit in colliders) {
+			Debug.Log("HIT");
+			Rigidbody2D body = hit.rigidbody2D;
+
+			Vector2 dir = transform.position - body.transform.position;
+			dir = dir.normalized;
+			Debug.Log("DISTANCE: " + dir);
+			body.AddForce(dir * force);
+		}
+	}
+
+	void OnCollisionEnter2D() {
+
+	}
+}

209ec8498d7830542ce092f07ae8c46882d21bdc
jlhirond <jlhirond@ualberta.ca>
2014-03-07 11:03:36 -0700

    Added a working credits scene, it is only lacking connections from the last scene and an exit after completing. Could be connected to Main Menu UI I suppose.

---
+++ Assets/Scripts/Credits.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+ 
+public class Credits : MonoBehaviour
+{
+    public string[] intro;
+    public float off;
+    public float speed = 100;
+    public GUIStyle font;
+ 
+    public void OnGUI()
+    {
+	//font = new GUIStyle();
+	//font.fontSize = 20;
+	//font.fontStyle = FontStyle.Bold;
+        off -= Time.deltaTime * speed;
+        for (int i = 0; i < intro.Length; i++)
+        {
+            float roff = (intro.Length*+30) - (i*20 - off);
+            float alph = Mathf.Sin((roff/Screen.height)*180*Mathf.Deg2Rad);
+            GUI.color = new Color(1,1,1,alph);
+            GUI.Label(new Rect(0,roff,Screen.width, 100),intro[i],font);
+            GUI.color = new Color(1,1,1,1);
+        }
+    }
+ 
+}

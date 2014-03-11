da7b8cc95d2106978e1f1c94f50722bac109b781
Solanar <and_555@hotmail.com>
2014-03-10 18:23:59 -0600

    spike enemy refactor

--- Assets/Scripts/Enemy/SpikeEnemy.cs
+++ Assets/Scripts/Enemy/SpikeEnemy.cs
@@ -3,42 +3,44 @@ using System.Collections;
 
 public class SpikeEnemy : MonoBehaviour 
 {
+	public float speed = 25;
+	public int health = 100;
+	public float damage = 2.0f;
+
 	public GenericEnemy self;
 	public Transform target;
 
-	private SpriteRenderer ren;
 	// A value to give the minimum amount of Torque when dying
 	public float deathSpinMin = -100f;
 	// A value to give the maximum amount of Torque when dying
 	public float deathSpinMax = 50f;
 
-	private bool dying;
+	private bool dying = false;
+	private SpriteRenderer ren;
+	private EnemyDeath death;
+	private EnemyMovement move;
 
-	public float speed;
-	// Use this for initialization
 	void Start () 
 	{
-		self = new GenericEnemy (this.gameObject, 100, speed, 2.0f);
+		self = new GenericEnemy (this.gameObject, health, speed, damage);
 		ren = transform.Find ("body").GetComponent<SpriteRenderer> ();
 
 		if (!target)
 			target = GameObject.FindWithTag ("Player").transform;
 
-		dying = false;
+		death = this.GetComponent<EnemyDeath> ();
+		move = this.GetComponent<EnemyMovement> ();
 	}
 
 	void FixedUpdate ()
 	{
-		if (self.health <= 0)
-			Death ();
-
 		if (dying)
 			return;
 
-		var dir = target.transform.position - this.transform.position;
-		dir = dir.normalized;
-		var force = dir * self.movementSpeed;
-		rigidbody2D.AddForce (force);
+		if (self.health <= 0)
+			death.Death (this, ren, deathSpinMin, deathSpinMax);
+
+		move.Move (this, target, self);
 	}
 
 	void OnCollisionEnter2D (Collision2D coll)
@@ -55,35 +57,7 @@ public class SpikeEnemy : MonoBehaviour
 		else if (coll.gameObject.tag == "Bullet"
 			&& coll.gameObject.GetComponent<EnemyProjectile> ().parent != this.gameObject)
 		{
-			Death ();
+			death.Death (this, ren, deathSpinMin, deathSpinMax);
 		}
 	}
-
-	public void Death ()
-	{
-		dying = true;
-
-		// Find all of the sprite renderers on this object and it's children.
-		SpriteRenderer[] otherRenderers = GetComponentsInChildren<SpriteRenderer> ();
-
-		// Disable all of them sprite renderers.
-		foreach (SpriteRenderer s in otherRenderers)
-			s.enabled = false;
-
-		// Re-enable the main sprite renderer and set it's sprite to the deadEnemy sprite.
-		ren.enabled = true;
-
-		// Allow the enemy to rotate and spin it by adding a torque.
-		rigidbody2D.fixedAngle = false;
-		rigidbody2D.AddTorque (Random.Range (deathSpinMin, deathSpinMax));
-
-		// Find all of the colliders on the gameobject and set them all to be triggers.
-		Collider2D[] cols = GetComponents<Collider2D> ();
-		foreach (Collider2D c in cols)
-			c.isTrigger = true;
-
-		Invoke ("Remove", 2.0f);
-	}
-
-	private void Remove () { Destroy (gameObject); }
 }

00aff72b17a4d3dcc70fb58202f31a7525349290
Solanar <and_555@hotmail.com>
2014-03-10 18:24:31 -0600

    move movement out of enemies

---
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement : MonoBehaviour 
+{
+	public void Move (MonoBehaviour move, Transform target, GenericEnemy self)
+	{
+		Vector3 dir = target.transform.position - move.transform.position;
+		dir = dir.normalized;
+		Vector2 force = dir * self.movementSpeed;
+		rigidbody2D.AddForce (force);
+	}
+}

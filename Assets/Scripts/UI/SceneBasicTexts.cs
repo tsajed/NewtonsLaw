061fac7f76370f51df70a8e4570c00a517bb11ef
Tanvir Sajed <tsajed@gmail.com>
2014-03-25 23:21:35 -0600

    Added tutorial texts for scene Basic

---
+++ Assets/Scripts/UI/SceneBasicTexts.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneBasicTexts : MonoBehaviour {
+
+	string tutorialString = "Welcome to the tutorial screen. Use the left mouse, aim it on " +
+		"an enemy and click it to push it away.";
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI() {
+		tutorialString = GUI.TextArea(new Rect(0, Screen.height - 50, Screen.width, 50), tutorialString);
+	}
+}

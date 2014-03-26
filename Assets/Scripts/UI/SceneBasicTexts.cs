ff9e0ef9a2c310d2c778846d91a2a983cc1d8932
Tanvir Sajed <tsajed@gmail.com>
2014-03-25 23:38:31 -0600

    Enemies spawn too fast after initial delay of 10 seconds. Needs to be fixed in spawner.cs

--- Assets/Scripts/UI/SceneBasicTexts.cs
+++ Assets/Scripts/UI/SceneBasicTexts.cs
@@ -4,7 +4,9 @@ using System.Collections;
 public class SceneBasicTexts : MonoBehaviour {
 
 	string tutorialString = "Welcome to the tutorial screen. Use the left mouse, aim it on " +
-		"an enemy and click it to push it away.";
+		"an enemy and click it to push it away. Click on the right mouse button to pull it toward" +
+		" the player. The objects that are spawning are basic enemies and they don't hurt you. You" +
+		" can push or pull them to your advantage. Click on the Tutorial 2 button for next tutorial";
 	// Use this for initialization
 	void Start () {
 	
@@ -16,6 +18,11 @@ public class SceneBasicTexts : MonoBehaviour {
 	}
 
 	void OnGUI() {
-		tutorialString = GUI.TextArea(new Rect(0, Screen.height - 50, Screen.width, 50), tutorialString);
+		tutorialString = GUI.TextArea(new Rect(0, Screen.height - 50, Screen.width - 150, 50), tutorialString);
+		if (GUI.Button(new Rect(Screen.width - 150, Screen.height - 50, 150, 50), "Tutorial 2")) {
+			Debug.Log("Clicked the button with text");
+			Application.LoadLevel(Application.loadedLevel + 1);
+		}
 	}
+
 }

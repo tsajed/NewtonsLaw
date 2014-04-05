1dd1b181a5760b557fc2b91a84937439bb8b181b
Eric Lam <elam3@ualberta.ca>
2014-04-05 14:23:23 -0600

    Removed automatically going to the next level after saving the player's level score.

--- Assets/Scripts/SaveScoreLevel.cs
+++ Assets/Scripts/SaveScoreLevel.cs
@@ -68,13 +68,6 @@ public class SaveScoreLevel : MonoBehaviour {
 				break;
 			}
 		}
-		
-		// Go to Next Stage
-		int index = Application.loadedLevel + 1;
-		if(index < Application.levelCount) 
-			Application.LoadLevel(index);
-	 	else
-	 		Application.LoadLevel("StartScreen");
 
 	}
 

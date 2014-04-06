5bd489a69a09ccc2cd9888f2ccac47ee6f0214d2
Eric Lam <elam3@ualberta.ca>
2014-04-06 13:35:41 -0600

    Level Select basic implementation done.

---
+++ Assets/Scripts/UI/LoadLevel.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Load the clicked Level
+public class LoadLevel : MonoBehaviour 
+{
+
+	void OnMouseDown()
+	{
+		// Get the level number from the name
+		string[] levelNumber = gameObject.name.Split(char.Parse(" "));
+
+		Application.LoadLevel("Scene " + levelNumber[1]);
+
+	}
+}

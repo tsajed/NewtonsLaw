4a79e005da627108200e38846b92fd682d9bf11c
Eric Lam <elam3@ualberta.ca>
2014-04-06 13:13:42 -0600

    Started Implementation of Level Select. Renamed LevelIcon prefab to LevelScoreIcon. Created another prefab for making icons for Level Select.

---
+++ Assets/Scripts/UI/LevelSelect.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelSelect : MonoBehaviour 
+{
+	public Sprite hoverSprite;
+	public GameObject levelSelect;	// Made this public cause Find cannot find inactive objects
+
+	private SpriteRenderer ren;
+	private Sprite normalSprite;
+	private GameObject startMenu;
+
+	void Awake() 
+	{
+		ren = gameObject.GetComponent<SpriteRenderer>();
+		normalSprite = ren.sprite;
+
+		startMenu = GameObject.Find("UI");
+	}
+
+    void OnMouseEnter() 
+    {
+    	ren.sprite = hoverSprite;
+    }
+
+    void OnEnable() 
+    {
+    	ren.sprite = normalSprite;
+    }
+
+    void OnMouseExit() 
+    {
+    	ren.sprite = normalSprite;
+    }
+
+    void OnMouseDown() 
+    {
+    	startMenu.SetActive(false);
+    	levelSelect.SetActive(true);
+    }
+}

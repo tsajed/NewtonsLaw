341cd931d177739319d158f9fc8b26087990ad21
Zach Lubinski <lubinski@ualberta.ca>
2014-04-06 13:02:56 -0600

    Added an effect that plays on the spawner before an enemy spawns.

--- Assets/Scripts/Example Scripts/Spawner.cs
+++ Assets/Scripts/Example Scripts/Spawner.cs
@@ -5,20 +5,31 @@ public class Spawner : MonoBehaviour
 {
 	public float spawnTime = 5f;		// The amount of time between each spawn.
 	public float spawnDelay = 3f;		// The amount of time before spawning starts.
+	public float effectTime = 3f;		// The amount of time the spawn warning effect plays for.
 	public GameObject[] enemies;		// Array of enemy prefabs.
+	ParticleSystem particles;
 
 
 	void Start ()
 	{
 		// Start calling the Spawn function repeatedly after a delay .
 		InvokeRepeating("Spawn", spawnDelay, spawnTime);
+		particles = GetComponentInChildren<ParticleSystem> ();
 	}
 
 
 	void Spawn ()
 	{
+		Invoke ("Create", effectTime);
+
+		particles.Play ();
+	}
+	void Create ()
+	{
 		// Instantiate a random enemy.
 		int enemyIndex = Random.Range(0, enemies.Length);
 		Instantiate(enemies[enemyIndex], transform.position, transform.rotation);
+		particles.Stop ();
+
 	}
 }

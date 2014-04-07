ea15fa15d71715db2a940050f3562f6ce6dc6bd9
Eric Lam <elam3@ualberta.ca>
2014-04-07 13:31:32 -0600

    Added saving Player Name. Currently saves when pressing Back button. But later on will have a "Save" button that will do the saving instead. And later on deal with issue of refilling the GUI text inputs when going back again to the settings.

---
+++ Assets/Scripts/UI/SaveOptions.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SaveOptions : MonoBehaviour 
+{
+
+    private string playerName;
+
+    void Awake()
+    {
+    	playerName = PlayerPrefs.GetString("PlayerName", "Unknown");
+    }
+
+    void OnGUI() 
+    {
+    	GUI.Label(new Rect(Screen.width/2 - 200, 100, 200, 20), "Player Name:");
+        playerName = GUI.TextField(new Rect(Screen.width/2 - 100, 100, 200, 20), playerName, 25);
+    }
+
+	void OnMouseDown()
+	{
+		// Save the Player Name
+        if(playerName != "")
+		  PlayerPrefs.SetString("PlayerName", playerName);
+	}
+}

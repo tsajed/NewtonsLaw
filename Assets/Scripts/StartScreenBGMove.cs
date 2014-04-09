0ed2e33f5482fc5e27f092f7b98ff8cf6d4b0b27
Eric Lam <elam3@ualberta.ca>
2014-04-08 23:05:34 -0600

    Made a more general script for background movement. Needs more work.

--- Assets/Scripts/StartScreenBGMove.cs
+++ Assets/Scripts/StartScreenBGMove.cs
@@ -1,35 +1,35 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class BackgroundMenu : MonoBehaviour {
+public class StartScreenBGMove : MonoBehaviour 
+{
 
 	public Vector3 start;
 	public Vector3 end;
 	public float speed = 10.0f;
 
-	private Transform group1;
 	private float journeyLength;
 	private float startTime;
 	private bool finished;
 
 	// Use this for initialization
-	void Start () {
-		group1 = GameObject.Find("Group1").transform;
-		
+	void Start () 
+	{		
 		startTime = Time.time;
 		journeyLength = Vector3.Distance(start, end);
 		//InvokeRepeating("MovePlayer", 2, 15.0f);
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
 	   	float distCovered = (Time.time - startTime) * speed;
         float fracJourney = distCovered / journeyLength;
 
-        if(group1.position != end) {
-			group1.position = Vector3.Lerp(start, end, fracJourney);
-		} else if(group1.position == end) {
-			group1.position = start;
+        if(transform.position != end) {
+			transform.position = Vector3.Lerp(start, end, fracJourney);
+		} else if(transform.position == end) {
+			transform.position = start;
 			startTime = Time.time;
 		}
 

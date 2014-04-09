0cabf96e0008939f2253e4d156657dce19afdc21
Zach Lubinski <lubinski@ualberta.ca>
2014-04-08 23:45:45 -0600

    Fixed issue where particles would appear behind planets.

--- Assets/Scripts/Example Scripts/SetParticleSortingLayer.cs
+++ Assets/Scripts/Example Scripts/SetParticleSortingLayer.cs
@@ -4,11 +4,12 @@ using System.Collections;
 public class SetParticleSortingLayer : MonoBehaviour
 {
 	public string sortingLayerName;		// The name of the sorting layer the particles should be set to.
-
+	public int sortingLayerNumber;
 
 	void Start ()
 	{
 		// Set the sorting layer of the particle system.
 		particleSystem.renderer.sortingLayerName = sortingLayerName;
+		particleSystem.renderer.sortingOrder = sortingLayerNumber;
 	}
 }

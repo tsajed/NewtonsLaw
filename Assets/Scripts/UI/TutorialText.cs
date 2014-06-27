812b712e91827529e3be08a99887694ead933cde
Eric Lam <elam3@ualberta.ca>
2014-06-27 16:38:47 -0600

    - Edited Main Camera prefab so that it doesn't have the disabled spawners attached to them anymore - Updated every scene in here with the edited Main Camera prefab - Reorganized the Tutorial Scenes' hierarchies. (Deleted unused spawners, placed backgrounds into one parent object, etc) - SceneLaser had no spawner, so I added one in that will spawn spike enemies, laser enemies, and the shooting enemies. - Refactored the Tutorial scripts into one general script called TutorialText.cs and removed the older Tutorial scripts

---
+++ Assets/Scripts/UI/TutorialText.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TutorialText : MonoBehaviour {
+
+	public string tutorialString = "";
+	public string tutorialButton = "";
+
+	void OnGUI() {
+		GUI.skin.textArea.fontSize = 18;
+		GUI.TextArea(new Rect(200, Screen.height - 150, Screen.width - 350, 200), tutorialString);
+		GUI.FocusControl("");
+		if (GUI.Button(new Rect(Screen.width - 150, Screen.height - 50, 150, 50), tutorialButton)) {
+			Debug.Log("Clicked the button with text");
+			Application.LoadLevel(Application.loadedLevel + 1);
+		}
+	}
+}

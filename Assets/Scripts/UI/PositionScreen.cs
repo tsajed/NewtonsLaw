0132dc8237c44103e4fb73d1c53007d7f213c96e
Zach Lubinski <lubinski@ualberta.ca>
2014-07-03 15:26:09 -0600

    Added a background for timer and script for positioning ui elements. Refactored newton to use the new script.

---
+++ Assets/Scripts/UI/PositionScreen.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PositionScreen : MonoBehaviour {
+	public float x;
+	public float y;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 p1 = Camera.main.ViewportToWorldPoint(new Vector3(x,y,0));
+		p1.z = 0f;
+		gameObject.transform.position = p1;
+	}
+}

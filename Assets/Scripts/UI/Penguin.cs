03ffbbe6ae96f2e57d1d4459c2354cdb1af97a8c
jlhirond <jlhirond@ualberta.ca>
2015-04-23 19:03:02 -0600

    no message

--- Assets/Scripts/UI/Penguin.cs
+++ Assets/Scripts/UI/Penguin.cs
@@ -3,22 +3,22 @@ using System.Collections;
 
 public class Penguin : MonoBehaviour {
 
-        public AudioClip penguin;
+	public AudioClip penguin;
 	public AudioClip explosion;
 	public AudioClip fire;
 	
 	// Use this for initialization
 	IEnumerator Start () 
 	{
-	     	   yield return new WaitForSeconds (0.4f);
-		   AudioSource.PlayClipAtPoint(penguin, GameObject.Find("Main Camera").transform.position);
-	     	   yield return new WaitForSeconds (2.5f);
-	     	   AudioSource.PlayClipAtPoint(explosion, GameObject.Find("Main Camera").transform.position);
-	     	   yield return new WaitForSeconds (0.5f);
-	 	   AudioSource.PlayClipAtPoint(fire, GameObject.Find("Main Camera").transform.position);
-		   yield return new WaitForSeconds (2);
-		   Application.LoadLevel ("StartScreen");
-
+		yield return new WaitForSeconds (0.4f);
+		AudioSource.PlayClipAtPoint(penguin, GameObject.Find("Main Camera").transform.position);
+		yield return new WaitForSeconds (3.1f);
+		AudioSource.PlayClipAtPoint(explosion, GameObject.Find("Main Camera").transform.position);
+		yield return new WaitForSeconds (0.5f);
+		AudioSource.PlayClipAtPoint(fire, GameObject.Find("Main Camera").transform.position);
+		yield return new WaitForSeconds (2);
+		yield return new WaitForSeconds (3);
+		Application.LoadLevel ("StartScreen");
 	}
     void OnMouseDown() {
    	Application.LoadLevel ("StartScreen");
